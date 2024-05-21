90ac2cb14e555b038b76087b94ff6a085f4d12cb
Oways Asi <owayslfkf@gmail.com>
2024-05-21 15:12:24 +0300

    subTask / added table cell component

---
+++ src/Components/alpha-table-cell/alpha-table-cell.tsx
@@ -0,0 +1,22 @@
+import { useId } from "react";
+import { TableCell, tableCellClasses } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.secondary.main,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+export function AlphaTableCell({ children, ...restProps }) {
+  const Id = useId();
+  return (
+    <StyledTableCell key={Id} {...restProps}>
+      {children}
+    </StyledTableCell>
+  );
+}
